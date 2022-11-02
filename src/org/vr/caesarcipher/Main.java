11bfafd8010da00010dca31d1b46ce24a3a2efdd
Viktor R <vr837017@gmail.com>
2022-11-02 10:57:53 +0300

    add main functional, add commandReader method

--- src/org/vr/caesarcipher/Main.java
+++ src/org/vr/caesarcipher/Main.java
@@ -1,7 +1,29 @@
 package org.vr.caesarcipher;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
 
+        System.out.println(Arrays.toString(args));
+
+        if (args.length > 0) {
+            commandReader(args);
+        }
+
+    }
+
+    public static void commandReader(String[] args) {
+        String command = args[0];
+
+        if (command.equals("encode")) {
+            System.out.println("encode detected");
+        } else if (command.equals("decode")) {
+            System.out.println("decode detected");
+        } else if (command.equals("bruteForce")) {
+            System.out.println("bruteForce detected");
+        } else {
+            throw new IllegalArgumentException(String.format("Unknown command %s", command));
+        }
     }
 }
