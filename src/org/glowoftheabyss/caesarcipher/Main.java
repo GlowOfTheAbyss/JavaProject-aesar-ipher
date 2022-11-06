10f26d6be70a4825c5901ae41edabae951d1d86a
Viktor R <vr837017@gmail.com>
2022-11-06 23:01:03 +0300

    rename package, remove CommandReader functional

---
+++ src/org/glowoftheabyss/caesarcipher/Main.java
@@ -0,0 +1,10 @@
+package org.glowoftheabyss.caesarcipher;
+
+public class Main {
+    public static void main(String[] args) {
+
+        CommandReader commandReader = new CommandReader();
+        commandReader.Read(args);
+
+    }
+}
