a28bcbff905e58e46873eaaebfcb849a2cce7b01
Viktor R <vr837017@gmail.com>
2022-11-11 15:42:32 +0300

    fixed a bug with the generation of names

--- src/org/glow/caesarcipher/PathGenerator.java
+++ src/org/glow/caesarcipher/PathGenerator.java
@@ -6,26 +6,37 @@ public class PathGenerator {
 
     public Path makeOutPathEncode (String sourcePath) {
 
-        StringBuilder sb = new StringBuilder(sourcePath);
-        sb.insert(sourcePath.length() - 4, "(encoded)");
+        StringBuilder sb = createStringBuilderRemovingBrackets(sourcePath);
+        sb.insert(sb.lastIndexOf("."), "(encoded)");
 
         return Path.of(sb.toString());
     }
 
     public Path makeOutPathDecode (String sourcePath) {
 
-        StringBuilder sb = new StringBuilder(sourcePath);
-        sb.insert(sourcePath.length() - 4, "(decoded)");
+        StringBuilder sb = createStringBuilderRemovingBrackets(sourcePath);
+        sb.insert(sb.lastIndexOf("."), "(decoded)");
 
         return Path.of(sb.toString());
     }
 
     public Path makeOutPathBruteForce (String sourcePath, int key) {
 
-        StringBuilder sb = new StringBuilder(sourcePath);
-        sb.insert(sourcePath.length() - 4, "(decoded key " + key + ")");
+        StringBuilder sb = createStringBuilderRemovingBrackets(sourcePath);
+        sb.insert(sb.lastIndexOf("."), "(decoded key " + key + ")");
 
         return Path.of(sb.toString());
+    }
+
+    private StringBuilder createStringBuilderRemovingBrackets (String sourcePath) {
+
+        StringBuilder sb = new StringBuilder(sourcePath);
+
+        if (sb.toString().contains("(") && sb.toString().contains(")")) {
+            sb.replace(sb.lastIndexOf("("), sb.lastIndexOf(")") + 1, "");
+        }
+
+        return sb;
     }
 
 }
